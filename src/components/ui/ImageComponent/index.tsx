5a52983f8aa189ca48fd67348ce1d27e97afd17a
ZakariaBenali <neonbenali@gmail.com>
2022-11-30 00:49:55 +0100

    feat: Lazy Load images in Search results

--- src/components/ui/ImageComponent/index.tsx
+++ src/components/ui/ImageComponent/index.tsx
@@ -1,4 +1,4 @@
-import { ImgHTMLAttributes, useState } from 'react';
+import { ImgHTMLAttributes, useCallback, useEffect, useRef, useState } from 'react';
 import { ImageLoader } from '../Loaders/ImageLoader';
 
 interface IProps extends Omit<ImgHTMLAttributes<HTMLImageElement>, 'onLoad'> {
@@ -9,13 +9,38 @@ interface IProps extends Omit<ImgHTMLAttributes<HTMLImageElement>, 'onLoad'> {
 
 const ImageComponent: React.FC<IProps> = ({ className, src, alt, withLoader, ...rest }) => {
 	const [isLoading, setIsLoading] = useState(true);
+	const imageRef = useRef(null);
+	const [imgSrc, setImgSrc] = useState('');
+
+	const callback = useCallback(
+		(entries: IntersectionObserverEntry[]) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					setImgSrc(src);
+				}
+			});
+		},
+		[src],
+	);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(callback);
+		if (imageRef.current) {
+			observer.observe(imageRef.current);
+		}
+		return () => {
+			observer.disconnect();
+		};
+	}, [callback]);
+
 	return (
 		<div className="relative h-full w-full">
 			{withLoader && isLoading && <ImageLoader />}
 			<img
 				className={className}
-				src={src}
+				src={imgSrc}
 				alt={alt}
+				ref={imageRef}
 				onLoad={() => {
 					setIsLoading(false);
 				}}
