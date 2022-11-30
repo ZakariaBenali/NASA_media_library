e94d9cbc4f90448912918cfb3fd767987cad5eee
ZakariaBenali <neonbenali@gmail.com>
2022-11-30 10:48:31 +0100

    feat: wrap notfound image with a fixed dimensions container to prevent CLS

--- src/components/ui/NotFound/index.tsx
+++ src/components/ui/NotFound/index.tsx
@@ -13,7 +13,9 @@ const NotFound: React.FC<IProps> = ({ className }) => {
 				className,
 			)}
 		>
-			<img className="w-full max-w-xl" src={NotFoundIllustration} alt="Not Found" />
+			<div className="w-full max-w-xl flex-1">
+				<img className="w-full h-full object-contain" src={NotFoundIllustration} alt="Not Found" />
+			</div>
 			<h3 className="font-bold text-4xl md:text-8xl mb-6">Oops!</h3>
 			<h3 className="font-bold text-xl max-w-lg md:max-w-3xl md:text-3xl mb-5">
 				The page you&apos;re looking for can&apos;t be found or another error occured.
